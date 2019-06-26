fdb8f91e150ee3b4795e6036b2f4c9d53d51a3ad
Matthew Mizielinski <matthew.mizielinski@gmail.com>
2019-06-26 21:48:10 +0100

    Update neopixel_radio.js

--- neopixel_and_sensors/neopixel_radio.js
+++ neopixel_and_sensors/neopixel_radio.js
@@ -1,10 +1,21 @@
 // Need to import Neopixel extension first.
 function get_pitch () {
-    pitch = Math.map(input.rotation(Rotation.Pitch), 0, 360, 0, 120)
+    pitch = Math.map(input.rotation(Rotation.Pitch), -180, 180, 0, 120)
 }
 function get_roll () {
-    roll = Math.map(input.rotation(Rotation.Roll), 0, 360, 0, 120)
+    roll = Math.map(input.rotation(Rotation.Roll), -180, 180, 0, 120)
 }
+
+input.onButtonPressed(Button.A, function () {
+    radio_group = 1
+    basic.showNumber(radio_group)
+    radio.setGroup(radio_group)
+})
+input.onButtonPressed(Button.B, function () {
+    radio_group = 2
+    basic.showNumber(radio_group)
+    radio.setGroup(radio_group)
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "pitch") {
         received_pitch = value
@@ -12,12 +23,13 @@ radio.onReceivedValue(function (name, value) {
         received_roll = value
     }
 })
+let radio_group = 0
 let received_roll = 0
 let received_pitch = 0
 let roll = 0
 let pitch = 0
-radio.setGroup(1)
 let strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Sad)
 basic.forever(function () {
     get_pitch()
     radio.sendValue("pitch", pitch)
