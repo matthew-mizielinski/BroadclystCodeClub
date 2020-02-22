e9ac1427f6e60361f6ab509e0fb420d48e7cbc84
Matthew Mizielinski <matthew.mizielinski@gmail.com>
2020-02-22 14:39:54 +0000

    Create neopixel_template.py

---
+++ python_intro/neopixel_template.py
@@ -0,0 +1,71 @@
+# Documentation can be found via 
+# https://microbit-micropython.readthedocs.io
+
+# Load routines that allow us to use the microbit
+from microbit import *
+# load radio library if we need it
+# import radio
+# load neopixel library if we need it
+import neopixel
+
+
+def forever1():
+    """
+    Code to run again and again
+    """
+    pass
+
+
+def on_shake():
+    """
+    Code to run when the accelerometer detects the
+    microbit is being shaken
+    """
+    pass
+
+
+# on start code
+
+np_strip = neopixel.NeoPixel(pin0, 8)
+
+# Colours for the neopixels
+brightness = 100
+# colour = (red, green, blue)
+# Primary
+RED = (brightness, 0, 0)
+GREEN = (0, brightness, 0)
+BLUE = (0, 0, brightness)
+
+# Secondary
+YELLOW = (brightness // 2, brightness // 2, 0)
+MAGENTA = (brightness // 2, 0, brightness // 2)
+CYAN = (0, brightness // 2, brightness // 2)
+
+# Other
+WHITE = (brightness // 3, brightness // 3, brightness //3)
+BLACK = (0, 0, 0)
+
+ORANGE = (brightness * 2 // 3, brightness // 3, 0)
+PINK = (brightness * 2 // 3, 0, brightness // 3)
+VIOLET = (brightness // 3, 0, brightness * 2 // 3)
+INDIGO = (0, brightness // 3, brightness * 2 // 3)
+SPRING_GREEN = (0, brightness * 2 // 3, brightness // 3)
+
+colour_list = [
+   RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, WHITE
+]
+
+# Code to set initial colours
+
+
+# show  neopixel colours
+np_strip.show()
+
+# main program loop
+while True:
+    # forever code
+    forever1()
+
+    # shake
+    if accelerometer.current_gesture() == 'shake':
+        on_shake()
