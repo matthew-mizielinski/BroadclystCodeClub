f436fc1cae7b49833bbd77974a56a1453f6fe603
Matthew Mizielinski <matthew.mizielinski@gmail.com>
2020-02-23 12:02:40 +0000

    Create hot_potato.py

---
+++ python_intro/whole_class_demo/hot_potato.py
@@ -0,0 +1,71 @@
+# Documentation can be found via 
+# https://microbit-micropython.readthedocs.io
+
+# Load routines that allow us to use the microbit
+from microbit import *
+# load radio library if we need it
+import radio
+# load neopixel library if we need it
+# import neopixel
+
+
+def forever():
+    """
+    Code to run again and again
+    """
+    radio.send('Hot potato')
+    sleep(sleep_time)
+
+
+def on_button_A():
+    """
+    Code to run when button A is pressed on its own
+    """
+    sleep_time -= 1000
+
+
+def on_button_B():
+    """
+    Code to run when button B is pressed on its own
+    """
+    sleep_time += 1000
+
+
+def on_button_AB():
+    """
+    Code to run when button A+B are pressed together
+    """
+    sleep_time = 5000
+
+
+def on_shake():
+    """
+    Code to run when the accelerometer detects the
+    microbit is being shaken
+    """
+    pass
+
+
+# Code to run on start
+radio.on()
+# transmit on channel 99
+radio.config(group=99)
+
+sleep_time = 5000
+
+# end of on start code
+
+# main program loop
+while True:
+    # forever code
+    forever()
+    # buttons
+    if button_a.is_pressed() and button_b.is_pressed():
+        on_button_AB()
+    elif button_a.is_pressed():
+        on_button_A()
+    elif button_b.is_pressed():
+        on_button_B()
+    # shake
+    if accelerometer.is_gesture('shake'):
+        on_shake()
