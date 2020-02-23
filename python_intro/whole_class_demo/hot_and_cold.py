db98f162f5b6f60dc95355eae5cf5297a61c2a59
Matthew Mizielinski <matthew.mizielinski@gmail.com>
2020-02-23 12:05:41 +0000

    remove buttons code for simplicity

--- python_intro/whole_class_demo/hot_and_cold.py
+++ python_intro/whole_class_demo/hot_and_cold.py
@@ -32,35 +32,6 @@ def update_display(strength):
         display.set_pixel(x, y, 9)
 
 
-def on_button_A():
-    """
-    Code to run when button A is pressed on its own
-    """
-    pass
-
-
-def on_button_B():
-    """
-    Code to run when button B is pressed on its own
-    """
-    pass
-
-
-def on_button_AB():
-    """
-    Code to run when button A+B are pressed together
-    """
-    pass
-
-
-def on_shake():
-    """
-    Code to run when the accelerometer detects the
-    microbit is being shaken
-    """
-    pass
-
-
 # Code to run on start
 radio.on()
 # listen on channel 99
@@ -72,10 +43,4 @@ radio.config(group=99)
 while True:
     # forever code
     forever()
-    # buttons
-    if button_a.is_pressed() and button_b.is_pressed():
-        on_button_AB()
-    elif button_a.is_pressed():
-        on_button_A()
-    elif button_b.is_pressed():
-        on_button_B()
+
